c0697653e03d6c184272f2e9e5a670926cd99e29
aaronfozzy <Aaron260802@hotmail.co.uk>
2022-03-22 15:32:21 +0000

    Changes

--- php_scripts/delete-process.php
+++ php_scripts/delete-process.php
@@ -2,9 +2,9 @@
 include_once 'database.php';
 $sql = "DELETE FROM playlist WHERE id='" . $_GET["id"] . "'";
 if (mysqli_query($conn, $sql)) {
-	echo "Record deleted successfully";
+echo "Record deleted successfully";
 } else {
-	echo "Error deleting record: " . mysqli_error($conn);
+echo "Error deleting record: " . mysqli_error($conn);
 }
 mysqli_close($conn);
 ?>
